cc6cdb07c00a375a51922faf05568b1a3611f314
Steve.Aristotle <way0utwest@hotmail.com>
2015-12-14 20:34:42 -0700

    Day 3 puzzle 1
    First puzzle working using a dictionary and iterating through.

---
+++ Day 3/day3_a.py
@@ -0,0 +1,49 @@
+import sys
+
+def house_count(directions):
+    deliveries = {(0,0):1}
+    x = 0
+    y = 0
+    for direction in directions:
+        if direction == '>':
+            x += 1
+            if (x,y) in deliveries:
+                current = deliveries[(x,y)]
+                deliveries[(x,y)] = current + 1
+            else:
+                deliveries[(x,y)] = 1
+        if direction == '<':
+            x -= 1
+            if (x,y) in deliveries:
+                current = deliveries[(x,y)]
+                deliveries[(x,y)] = current + 1
+            else:
+                deliveries[(x,y)] = 1
+        if direction == '^':
+            y += 1
+            if (x,y) in deliveries:
+                current = deliveries[(x,y)]
+                deliveries[(x,y)] = current + 1
+            else:
+                deliveries[(x,y)] = 1
+        if direction == 'v':
+            y -= 1
+            if (x,y) in deliveries:
+                current = deliveries[(x,y)]
+                deliveries[(x,y)] = current + 1
+            else:
+                deliveries[(x,y)] = 1
+    print(str(len(deliveries)))
+
+    return
+
+if __name__ == '__main__':
+    filename = "input.txt"
+    with open(filename) as package_file:
+        for line in package_file:
+            houses = house_count(line)
+else:
+    filename = "input.txt"
+    with open(filename) as package_file:
+        for line in package_file:
+            houses = house_count(line)
