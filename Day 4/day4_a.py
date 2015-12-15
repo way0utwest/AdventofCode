41f3e710b0f2c406df1d25eaba0b1f3e078b6398
Steve.Aristotle <way0utwest@hotmail.com>
2015-12-14 21:11:17 -0700

    Day 4 A solution
    short code to calculate MD5 hashes and brute force them

---
+++ Day 4/day4_a.py
@@ -0,0 +1,11 @@
+import hashlib
+
+secret_key = "iwrupvqb"
+counter = 0
+hash = str(secret_key)
+while hash.startswith('00000') == False:
+    counter += 1
+    hash = hashlib.md5(str(secret_key + str(counter)).encode()).hexdigest()
+
+print(secret_key + str(counter))
+print(str(counter))
