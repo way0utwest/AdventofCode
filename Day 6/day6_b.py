d5a935ae0c217f3a51f177bd4580f35ac25c7af3
Steve.Aristotle <way0utwest@hotmail.com>
2015-12-21 10:26:29 -0500

    Refactored loops
    Removed WhIle loops, tried FOR loops instead as iterators. Seems cleaner

--- Day 6/day6_b.py
+++ Day 6/day6_b.py
@@ -6,27 +6,13 @@ def initialize_lights(grid):
         for y in range(0,1000):
             grid[(x,y)] = 0
 
-def lights_on(startx, starty, endx, endy, grid):
-    while startx <= endx:
-        while starty <= endy:
-            grid[(startx,starty)] = 1
-            starty += 1
-        startx += 1
-
-def lights_off(startx, starty, endx, endy, grid):
-    pass
-
 def lights_switch(startx, starty, endx, endy, grid, toggle):
     y = starty
     while startx <= endx:
         while starty <= endy:
-            if toggle == 2:
-                if grid[(startx,starty)] == 1:
-                    grid[(startx,starty)] = 0
-                else:
-                    grid[(startx,starty)] = 1
-            else:
-                grid[(startx,starty)] = toggle
+            grid[(startx,starty)] += toggle
+            if grid[(startx,starty)] < 0:
+                grid[(startx,starty)] = 0
             starty += 1
         starty = y
         startx += 1
@@ -40,7 +26,7 @@ def switch_lights(grid, instruction):
     if instruction.startswith(("turn off")):
         startx, starty = tokens[2].split(",")
         endx, endy = tokens[4].split(",")
-        lights_switch(int(startx), int(starty), int(endx), int(endy), grid, 0)
+        lights_switch(int(startx), int(starty), int(endx), int(endy), grid, -1)
     if instruction.startswith("toggle"):
         startx, starty = tokens[1].split(",")
         endx, endy = tokens[3].split(",")
@@ -51,8 +37,7 @@ def count_lights(grid):
     total = 0
     for x in range(0,1000):
         for y in range(0,1000):
-            if grid[(x,y)] == 1:
-                total += 1
+            total += grid[(x,y)]
     return total
 
 
@@ -62,6 +47,9 @@ grid = {}
 initialize_lights(grid)
 with open(filename) as package_file:
     for line in package_file:
-        print(line)
         switch_lights(grid, line)
 print("count: " + str(count_lights(grid)))
+
+# 14191263
+# 14687245
+# 14687245 - correct
