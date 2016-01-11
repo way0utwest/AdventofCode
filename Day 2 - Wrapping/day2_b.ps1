69fa201f65fe3b7ab518e6cd31016cc5b76a8f3c
way0utwest <sjones@dkranch.net>
2016-01-11 16:42:18 -0700

    Day 2 - PoSh
    Solutions for both day 2 puzzles in PoSh. Simple math, and much quicker
    to solve than in Python. That was interesting.

---
+++ Day 2 - Wrapping/day2_b.ps1
@@ -0,0 +1,13 @@
+﻿$sum = 0
+$file = "input.txt"
+$filename = Join-Path "D:\Documents\GitHub\AdventofCode\Day 2 - Wrapping" $file
+
+foreach ($line in [System.IO.File]::ReadLines($filename)) {
+    $dimensions = $line.split("x") | % {iex $_} | Sort-Object
+    $length = $dimensions[0]
+    $width = $dimensions[1]
+    $height = $dimensions[2]
+    $sum = $sum + ((2*$length*$width) + (2 * $length * $height) + (2 * $width * $height) + ($length * $width))
+}
+
+Write-Host("Total:" + $sum)
