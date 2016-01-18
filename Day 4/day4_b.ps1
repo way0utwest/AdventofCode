cf9e8a6e5e61ea413889d0477bcca97b97580b68
Steve.Aristotle <way0utwest@hotmail.com>
2016-01-18 11:47:01 -0700

    Day 4 - Powershell
    Both brute force checks in PS

---
+++ Day 4/day4_b.ps1
@@ -0,0 +1,21 @@
+﻿#calculate MD5 hashes
+$seed = 'iwrupvqb'
+$num = 300000
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+$utf8 = new-object -TypeName System.Text.UTF8Encoding
+
+while ($true) {
+
+  $hashstring = $seed + $num.ToString()
+
+  $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($hashstring)))
+  write-host("Trying " + $num.ToString() + "   - Hash:" + $hash.ToString())
+
+  if ($hash.ToString().Startswith("00-00-00")) {
+     break
+  }
+  $num += 1
+
+}
+
+write-host($num)
