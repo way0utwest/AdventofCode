682378b2d4873bf367e9fca4e637b16afb7cc349
Steve.Aristotle <way0utwest@hotmail.com>
2016-01-18 11:50:20 -0700

    Template for day 5. Stubbed out.
    Just a stub to read lines.

---
+++ Day 5/day5_a.ps1
@@ -0,0 +1,13 @@
+﻿#Day 5 - Strings
+
+$nicestrings = 0
+$file = "input.txt"
+$filename = Join-Path "C:\users\steve\documents\github\AdventofCode\Day 5" $file
+
+foreach ($line in [System.IO.File]::ReadLines($filename)) {
+   write-host($line)
+}
+
+
+
+write-host($nicestrings)
