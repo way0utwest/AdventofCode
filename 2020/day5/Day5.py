dfa5b95eeddd67ac09761268282782f5bedcfac8
Steve Jones <steve.jones@red-gate.com>
2021-04-13 10:01:11 -0600

    Day5 solution in Python

--- 2020/day5/Day5.py
+++ 2020/day5/Day5.py
@@ -1,9 +1,16 @@
-#day 5
-import pandas as pd
+# part 1
+file_handle = open('day5_data.txt', 'r')
+seatcodes = file_handle.readlines()
+
+part1 = 0
+seatval = []
+for code in seatcodes:
+    seatval.append(int(code.replace('B','1').replace('F','0').replace('L','0').replace('R','1'), 2))
+
+part1 = max(seatval)
+print("Part 1:", part1)
 
 
-Mydict = { 'Broncos': [5, 12], 'Chiefs': [14, 2], 'Chargers':[7,9], 'Raiders': [8,8]}
-
-df = pd.DataFrame(Mydict)
-
-print (df)
+part2 = 0
+j = [i for i in range(min(seatval), max(seatval)) if i not in seatval]
+print(j)
